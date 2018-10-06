7ad9ebeecc35e69b7cd4939a258915c09a951ee6
Rishabh42 <rishabhthaney@gmail.com>
2018-10-07 01:50:47 +0530

    added energy param

--- contracts/SMContract.sol
+++ contracts/SMContract.sol
@@ -6,22 +6,24 @@ contract SMContract {
       uint power;
       uint voltage;
       uint current;
+      uint energy;
       uint readingNo;
     }
 
     Reading[] public readingsArr;
 
-    function enterParams(uint power, uint voltage, uint current) public {
+    function enterParams(uint power, uint voltage, uint current, uint energy) public {
     Reading memory newReading = Reading({
       power: power,
       voltage: voltage,
       current: current,
+      energy: energy,
       readingNo: readingsArr.length
       });
     readingsArr.push(newReading);
     }
 
-    function getData(uint readingNo) public constant returns(uint, uint, uint) {
-        return (readingsArr[readingNo].power, readingsArr[readingNo].voltage, readingsArr[readingNo].current);
+    function getData(uint readingNo) public constant returns(uint, uint, uint, uint) {
+        return (readingsArr[readingNo].power, readingsArr[readingNo].voltage, readingsArr[readingNo].current, readingsArr[readingNo].energy);
     }
   }
