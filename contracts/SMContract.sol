5ba6355b0009adbafce55e7f5996d03811ec567b
Rishabh42 <rishabhthaney@gmail.com>
2018-11-13 16:13:16 +0530

    added owner, mapping instead of arr

--- contracts/SMContract.sol
+++ contracts/SMContract.sol
@@ -2,28 +2,38 @@ pragma solidity ^0.4.16;
 
 contract SMContract {
 
+    address private utility;
+
     struct Reading{
-      uint power;
-      uint voltage;
-      uint current;
-      uint energy;
-      uint readingNo;
+      address uAddr;
+      uint256 power;
+      uint256 voltage;
+      uint256 current;
+      uint256 energy;
+      uint256 readingNo;
     }
 
-    Reading[] public readingsArr;
+    mapping (uint256=> Reading) public _readings;
+    uint256[] public readingsArr;
 
-    function enterParams(uint power, uint voltage, uint current, uint energy) public {
-    Reading memory newReading = Reading({
-      power: power,
-      voltage: voltage,
-      current: current,
-      energy: energy,
-      readingNo: readingsArr.length
-      });
-    readingsArr.push(newReading);
+    constructor() public {
+        utility = msg.sender;
     }
 
-    function getData(uint readingNo) public constant returns(uint, uint, uint, uint) {
+    function newReading(uint256 readingNo, uint256 power, uint256 voltage, uint256 current, uint256 energy) public {
+    Reading storage _newReading = _readings[readingNo++];
+
+      _newReading.uAddr = msg.sender;
+      _newReading.power = power;
+      _newReading.voltage = voltage;
+      _newReading.current = current;
+      _newReading.energy = energy;
+      _newReading.readingNo = readingsArr.length++;
+      
+    readingsArr.push(readingNo);
+    }
+
+    /*function getData(uint readingNo) public constant returns(uint, uint, uint, uint) {
         return (readingsArr[readingNo].power, readingsArr[readingNo].voltage, readingsArr[readingNo].current, readingsArr[readingNo].energy);
-    }
+    }*/
   }
