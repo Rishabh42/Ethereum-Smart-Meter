b8c9840ed853fcf27ac1035f6b4b4d154d696d40
Rishabh42 <rishabhthaney@gmail.com>
2018-11-14 18:36:17 +0530

    created owner modifier

--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -1,23 +1,47 @@
-pragma solidity ^0.4.23;
+pragma solidity ^0.4.16;
 
-contract Migrations {
-  address public owner;
-  uint public last_completed_migration;
+contract SMContract {
 
-  constructor() public {
-    owner = msg.sender;
+    address private utility;
+    uint private counter;
+
+    struct Reading{
+      address uAddr;
+      uint256 power;
+      uint256 voltage;
+      uint256 current;
+      uint256 energy;
+      uint256 readingNo;
+    }
+
+    mapping (uint256=> Reading) public _readings;
+    uint256[] public readingsArr;
+
+    constructor() public {
+        utility = msg.sender;
+        counter = 1;
+    }
+    
+    modifier notUtility{
+        require(msg.sender != utility);
+        _;
+    }
+
+    function newReading( uint256 power, uint256 voltage, uint256 current, uint256 energy) notUtility public {
+    //Reading storage _newReading = _readings[readingNo++];
+
+      _readings[counter].uAddr = msg.sender;
+      _readings[counter].power = power;
+      _readings[counter].voltage = voltage;
+      _readings[counter].current = current;
+      _readings[counter].energy = energy;
+      _readings[counter].readingNo = readingsArr.length++;
+
+    readingsArr.push(counter);
+    counter++;
+    }
+
+    /*function getData(uint readingNo) public constant returns(uint, uint, uint, uint) {
+        return (readingsArr[readingNo].power, readingsArr[readingNo].voltage, readingsArr[readingNo].current, readingsArr[readingNo].energy);
+    }*/
   }
-
-  modifier restricted() {
-    if (msg.sender == owner) _;
-  }
-
-  function setCompleted(uint completed) public restricted {
-    last_completed_migration = completed;
-  }
-
-  function upgrade(address new_address) public restricted {
-    Migrations upgraded = Migrations(new_address);
-    upgraded.setCompleted(last_completed_migration);
-  }
-}
