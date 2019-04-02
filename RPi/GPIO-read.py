586f3f2ccbba986c044ea7d0e1268be53d9ee009
Rishabh Thaney <rishabhthaney@gmail.com>
2019-04-02 13:20:17 +0100

    Updated GPIO

--- RPi/GPIO-read.py
+++ RPi/GPIO-read.py
@@ -2,21 +2,26 @@ import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BOARD)
 
-pulse=11
+pulse=3
 
 GPIO.setup(pulse,GPIO.IN,pull_up_down=GPIO.PUD_UP)
 
 counter=0
 
 def energy_pulse(pulse):
-        global counter
-        if GPIO.input(pulse)> 0.5 :
-                counter +=1
+    global counter
+    if GPIO.input(pulse)> 0.5 :
+        counter +=1
+        print("energy count is:", counter)
 
-GPIO.add_event_detect(pulse, GPIO.rising, callback=energy_pulse)
+GPIO.add_event_detect(pulse, GPIO.RISING, callback=energy_pulse)
 
-for x in range (0,1000):
-	print("energy count is:", counter)
-	time.sleep(.1)
-
+done=False;
+while not done:
+    try:
+        time.sleep(4);
+        print("Updating");
+    except KeyboardInterrupt:
+        print("Program Ended");
+        done=True;
 GPIO.cleanup()
