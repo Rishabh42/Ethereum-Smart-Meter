197f99baf26d8a04c3e1ea9aa399f6a1c9f1f07f
rishabh <rishabhthaney@gmail.com>
2019-04-09 14:54:00 +0530

    modified contract_inst

--- RPi/contract_abi.py
+++ RPi/contract_abi.py
@@ -1,5 +1,85 @@
 abi = """[
- {
-  // A BIG LIST OF ABI INFO SPREAD ACROSS MULTIPLE DICTS
- }
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "energy",
+				"type": "uint256"
+			}
+		],
+		"name": "newReading",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"name": "_user",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"name": "_reading",
+				"type": "uint256"
+			}
+		],
+		"name": "readingEvent",
+		"type": "event"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "_readings",
+		"outputs": [
+			{
+				"name": "uAddr",
+				"type": "address"
+			},
+			{
+				"name": "energy_consumed",
+				"type": "uint256"
+			},
+			{
+				"name": "readingNo",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "readingsArr",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	}
 ]"""
